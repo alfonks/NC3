733f648602d755714a6fec640344f0f3982253cd
Alfonsus Kurniawan <48182208+alfonks@users.noreply.github.com>
2020-06-11 14:34:17 +0700

    Nano Challenge 3 Init

---
+++ NC3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC3
+//
+//  Created by Alfon on 11/06/20.
+//  Copyright © 2020 Alfon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
